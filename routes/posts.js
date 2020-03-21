5fbc086c332146f0bc67e341f84e5179df209c02
Kashish94 <kashishsinghlive@gmail.com>
2020-03-21 17:39:01 +0530

    posts created in db

--- routes/posts.js
+++ routes/posts.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const postsController = require("../controllers/posts_controller");
 
-router.get("/profile", postsController.profile);
+router.post("/create", postsController.create);
 
 module.exports = router;
