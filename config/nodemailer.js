ba1cf940cf6793cbfb2e43f38b24fe17d2340165
Kashish94 <kashishsinghlive@gmail.com>
2020-04-03 01:39:48 +0530

    sending HTML email templates

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,6 +1,7 @@
 const nodemailer = require("nodemailer");
-const ejs = require("ejs");
-const path = require("path");
+const ejs = require('ejs');
+const path = require('path')
+
 
 let transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -9,28 +10,28 @@ let transporter = nodemailer.createTransport({
     secure: false,
     auth: {
         user: 'kashishsinghlive@gmail.com',
-        pass: 'yjbbvfbnwkqrzgxf'
+        pass: 'fxisvvthvhxstsfd'
     }
 });
+
 
 let renderTemplate = (data, relativePath) => {
     let mailHTML;
     ejs.renderFile(
-        path.join(__dirname, "../views/mailers", relativePath),
+        path.join(__dirname, '../views/mailers', relativePath),
         data,
-        function(err, renderTemplate) {
-            if (err) {
-                console.log("error in rendering template");
-                return;
-            }
+        function(err, template) {
+            if (err) { console.log('error in rendering template', err); return }
+
             mailHTML = template;
         }
-    );
+    )
 
     return mailHTML;
-};
+}
+
 
 module.exports = {
     transporter: transporter,
     renderTemplate: renderTemplate
-};
+}
--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -1,23 +1,22 @@
-const nodeMailer = require("../config/nodemailer");
+const nodeMailer = require('../config/nodemailer');
+
 
 // this is another way of exporting a method
-exports.newComment = comment => {
-    console.log("inside newComment mailer", comment);
+exports.newComment = (comment) => {
+    let htmlString = nodeMailer.renderTemplate({ comment: comment }, '/comments/new_comment.ejs');
 
     nodeMailer.transporter.sendMail({
-            from: "ssjsr1975@gmail.com ",
-            to: comment.user.email,
-            subject: "New Comment Published!",
-            html: "<h1>Yup, your comment is now published!</h1>"
-        },
-        (err, info) => {
-            if (err) {
-                console.log("Error in sending mail", err);
-                return;
-            }
-
-            console.log("Message sent", info);
+        from: 'ssjsr1975@gmail.com',
+        to: comment.user.email,
+        subject: "New Comment Published!",
+        html: htmlString
+    }, (err, info) => {
+        if (err) {
+            console.log('Error in sending mail', err);
             return;
         }
-    );
-};
+
+        console.log('Message sent', info);
+        return;
+    });
+}
