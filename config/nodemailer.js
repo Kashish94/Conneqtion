efe7e66d80f4fb2fe4be28705ad34f62a1becb9a
Kashish94 <kashishsinghlive@gmail.com>
2020-04-02 18:09:45 +0530

    setting up nodemailer

---
+++ config/nodemailer.js
@@ -0,0 +1,36 @@
+const nodemailer = require("nodemailer");
+const ejs = require("ejs");
+const path = require("path");
+
+let transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: "false",
+    auth: {
+        user: "kashishsinghlive@gmail.com",
+        password: "fuck$Physics"
+    }
+});
+
+let renderTemplate = (data, relativePath) => {
+    let mailHTML;
+    ejs.renderFile(
+        path.join(__dirname, "../views/mailers", relativePath),
+        data,
+        function(err, renderTemplate) {
+            if (err) {
+                console.log("error in rendering template");
+                return;
+            }
+            mailHTML = template;
+        }
+    );
+
+    return mailHTML;
+};
+
+module.exports = {
+    transporter: transporter,
+    renderTemplate: renderTemplate
+};
