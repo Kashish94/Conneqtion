9454322956042585ad74da656897b49dddb17b84
Kashish94 <kashishsinghlive@gmail.com>
2020-04-04 21:18:57 +0530

    adding likes to post and comments

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -3,6 +3,8 @@ const Post = require("../models/post");
 const commentsMailer = require("../mailers/comments_mailer");
 const queue = require('../config/kue');
 const commentEmailWorker = require('../workers/comment_email_worker');
+const Like = require('../models/like');
+
 
 module.exports.create = async function(req, res) {
     try {
@@ -59,6 +61,7 @@ module.exports.destroy = async function(req, res) {
                 $pull: { comments: req.params.id }
             });
 
+            await Like.deleteMany({ likeable: comment._id, onModel: 'Comment' });
             // send the comment id which was deleted back to the views
             if (req.xhr) {
                 return res.status(200).json({
