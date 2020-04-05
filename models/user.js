03b21509fadf4b8a36d31e8f4eb8beb2a8d89112
Kashish94 <kashishsinghlive@gmail.com>
2020-04-05 15:45:41 +0530

    making friendships

--- models/user.js
+++ models/user.js
@@ -1,8 +1,8 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const multer = require("multer");
-const path = require("path");
-const AVATAR_PATH = path.join("/uploads/users/avatars");
+const multer = require('multer');
+const path = require('path');
+const AVATAR_PATH = path.join('/uploads/users/avatars');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -20,26 +20,33 @@ const userSchema = new mongoose.Schema({
     },
     avatar: {
         type: String
-    }
+    },
+    friendships: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Friendship'
+    }]
+
 }, {
     timestamps: true
 });
 
+
 let storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, path.join(__dirname, "..", AVATAR_PATH));
+        cb(null, path.join(__dirname, '..', AVATAR_PATH));
     },
     filename: function(req, file, cb) {
-        cb(null, file.fieldname + "-" + Date.now());
+        cb(null, file.fieldname + '-' + Date.now());
     }
 });
 
-//static methods
-userSchema.statics.uploadedAvatar = multer({ storage: storage }).single(
-    "avatar"
-);
+
+// static
+userSchema.statics.uploadedAvatar = multer({ storage: storage }).single('avatar');
 userSchema.statics.avatarPath = AVATAR_PATH;
 
-const User = mongoose.model("User", userSchema);
+
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
